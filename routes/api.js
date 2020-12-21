c4ff4fcd210ffcad7a0cc465e136cda69bc2c3ac
RavenJabhaJoseph <ravenjabhajoseph@gmail.com>
2020-12-21 12:41:07 +0530

    created a server, database using express and mongoDB now writing the routes

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/todos', (res, req, next) => {
+
+});
+
+router.post('/todos', (res, req, next) => {
+
+});
+
+router.delete('/todos/:id', (req, res, next) => {
+
+});
+
+module.exports = router;
